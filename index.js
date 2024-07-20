7f7ace8ec3f5014c7699862442a4ebf88993b4e5
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-20 21:45:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(record){
+    //check if win is present and return year
+    let win = record.find(obj => obj.result ==="W");
+    if (win){
+        return win.year;
+    }else
+        return undefined;
+}
+
+console.log(superbowlWin);
